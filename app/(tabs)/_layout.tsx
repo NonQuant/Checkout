572b6919863e8428e8c6b345c3e52c51d42a338a
NonQuant <husainov.21.04@gmail.com>
2024-04-05 18:45:46 +0500

    Updated icons for tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,55 @@
 import { Tabs } from "expo-router";
 import * as React from "react";
+import {
+  MaterialCommunityIcons,
+  Entypo,
+  Ionicons,
+  Feather,
+} from "@expo/vector-icons";
 
 const Layout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="scan" />
-      <Tabs.Screen name="receipts" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={24} color={color} />
+          ),
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="scan"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="qrcode-scan"
+              size={24}
+              color={color}
+            />
+          ),
+          title: "Scan",
+        }}
+      />
+      <Tabs.Screen
+        name="receipts"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="receipt-outline" size={24} color={color} />
+          ),
+          title: "Receipts",
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="user" size={24} color={color} />
+          ),
+          title: "Profile",
+        }}
+      />
     </Tabs>
   );
 };
